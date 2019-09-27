b8aabdda902234e7a72c8138313ce76d5ee44df7
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2019-09-27 19:01:56 +0900

    diary.sh完成

--- diary.sh
+++ diary.sh
@@ -8,4 +8,12 @@ if [ ! -d "$directory" ]; then
   mkdir "$directory"
 fi
 
-code "${directory}/$(date '+%Y-%m-%d').txt"
+# 日記ファイルの組み立て
+diaryfile="${directory}/$(date '+%Y-%m-%d').txt"
+
+# 日記ファイルがなければ(今日始めて書くならば)、先頭に日付を挿入
+if [ ! -e "$diaryfile" ]; then
+  date '+%Y/%m/%d' > "$diaryfile"
+fi
+
+code "$diaryfile"
